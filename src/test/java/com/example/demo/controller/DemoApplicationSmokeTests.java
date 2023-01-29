35b69ebfe37c82395385e3ed60f777400f9dbb99
Mohammed Khalique <khalique.mohammed@gmail.com>
2023-01-30 00:38:52 +0700

    first commit

---
+++ src/test/java/com/example/demo/controller/DemoApplicationSmokeTests.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoApplicationSmokeTests {
+
+	@Autowired
+	private EmployeeController employeeController;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(employeeController).isNotNull();
+	}
+}
