f41ad9836bb56c9a62cd0dd9d555622b52b0ff33
khalique123 <khalique.mohammed@gmail.com>
2023-01-31 10:37:25 +0700

    Update EmployeeConfiguration.java

--- src/main/java/com/example/demo/EmployeeConfiguration.java
+++ src/main/java/com/example/demo/EmployeeConfiguration.java
@@ -6,6 +6,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 
@@ -21,5 +30,21 @@ class EmployeeConfiguration {
 			Employee emp1 = new Employee("emp1", "t");
 			log.info("Preloading " + repository.save(emp1));
 		};
+	}
+	
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+		JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+
+		bean.setJndiName("java:comp/env/" + env.getProperty("spring.datasource.jndi-name"));
+		bean.setLookupOnStartup(false);
+		bean.setCache(true);
+		bean.setProxyInterface(DataSource.class);
+		bean.afterPropertiesSet();
+
+		return (DataSource) bean.getObject();
 	}
 } 
